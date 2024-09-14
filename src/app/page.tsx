31cc0b0233dbc5f621260bbb45de8f589d93e4d6
Igorrafael90 <rafaeligor804@gmail.com>
2024-09-14 20:13:03 -0300

    rotas-para-paginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@
 
 import { Personsop } from "@/data";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
-  const[search,setsearch] = React.useState('')
+  const [search, setsearch] = React.useState('')
   const searchlowercase = search.toLowerCase()
   const trips = Personsop.filter(trip => trip.Name.toLowerCase().includes(searchlowercase))
 
@@ -14,18 +15,20 @@ export default function Home() {
       <input type="search" value={search} onChange={(e) => setsearch(e.target.value)} className="w-1/2 h-8 mx-auto mt-2 bg-slate-300 rounded-xl border-blue-500 border-2" />
 
       <section className="w-full max-w-5xl mx-auto px-6 grid grid-cols-3 gap-8 mt-9">
-            {trips.map((trip) => (
-              <div key={trip.Name} className="bg-gray-600 h-80 rounded-md p-8 border-gray-600 border shadow-black shadow-lg hover:scale-110 hover:-translate-y-4 hover:border-red-500">
+        {trips.map((trip) => (
+          <Link href={`/person/${trip.Name.toLocaleLowerCase()}`} key={trip.Name} className="block">
+            <div className="bg-gray-600 h-80 rounded-md p-8 border-gray-600 border shadow-black shadow-lg hover:scale-110 hover:-translate-y-4 hover:border-red-500">
               <header className="flex items-center justify-between text-gray-300">
-                  <h1>{trip.Name}</h1>
-                  <h2>icone</h2>
+                <h1>{trip.Name}</h1>
+                <h2>icone</h2>
               </header>
               <p>h2 mais uma imagem</p>
               <p>{trip.description}</p>
-          </div>
-          ))}
-        </section>
-       
+            </div>
+          </Link>
+        ))}
+      </section>
+
     </main>
   );
 }
